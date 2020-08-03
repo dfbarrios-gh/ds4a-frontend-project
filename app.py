bedb36ad9f563edf2fef732670d3e9388f96e018
dffbarrios <dfbarrios@gmail.com>
2020-08-02 21:57:14 -0500

    Solving deploy issues

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import dash_html_components as html
 import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+#app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets, requests_pathname_prefix = '/flaskfront/')
+server = app.server
 app.title = 'DS4A 2020 Team35'
 
 #global
